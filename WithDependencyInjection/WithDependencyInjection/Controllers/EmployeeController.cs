264423f63fa7aa7703fed13948d6696e12a076f7
Pitambar Thapa <pitambar.thapa@logicabeans.com>
2023-09-26 12:01:24 +0545

    Adding Dot Net Core project with Dependency Injection

---
+++ WithDependencyInjection/WithDependencyInjection/Controllers/EmployeeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WithDependencyInjection.Repository;
+
+namespace WithDependencyInjection.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+        public EmployeeController(IEmployeeRepository employeeRepository) {
+            _employeeRepository = employeeRepository;
+        }
+        public JsonResult Index()
+        {
+            return Json(_employeeRepository.GetAll());
+        }
+
+        public JsonResult Details(int id)
+        {
+            return Json(_employeeRepository.GetById(id));
+        }
+    }
+}
---
+++ WithDependencyInjection/WithDependencyInjection/Repository/IEmployeeRepository.cs
@@ -0,0 +1,10 @@
+﻿using WithDependencyInjection.Models;
+
+namespace WithDependencyInjection.Repository
+{
+    public interface IEmployeeRepository
+    {
+        List<Employee> GetAll();
+        Employee GetById(int id);
+    }
+}
